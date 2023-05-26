1506def35348d70683dbb70426cc429075a80a0d
Joao Lobo <joao-lobo12@hotmail.com>
2023-05-26 13:52:48 -0300

    Limpeza menu

--- src/Menu/menu.c
+++ src/Menu/menu.c
@@ -17,7 +17,7 @@ int MenuInicial(FILE **arquivoDeEntrada)
             printf("\n\n=================================================================");
             printf("\nEntre com um dos comandos a seguir para:\n");
             printf("U) Indicar o fim de uma unidade de tempo;\n");
-            printf("I) Imprimir o estado atual do sistema;\n");
+            printf("I) Executar o proceso de impressão;\n");
             printf("M) Imprimir o tempo médio de resposta e finalizar o sistema.");
             printf("\n=================================================================\n");
         }
@@ -32,19 +32,6 @@ int MenuInicial(FILE **arquivoDeEntrada)
 
     } while (opcao != 1 && opcao != 2);
 
-    return opcao;
-}
-
-int MenuImpressaoProcesso()
-{
-    int opcao = 1;
-
-    printf("\n1) Não exibir nenhuma;");
-    printf("\n2) Variáveis do processo;");
-    printf("\n3) Instruções do processo;");
-    printf("\n4) Instruções e variáveis do processo;");
-    printf("\n>> Escolha a opção: ");
-    scanf(" %d", &opcao);
     return opcao;
 }
 
--- src/Menu/menu.h
+++ src/Menu/menu.h
@@ -4,7 +4,4 @@
 #include <math.h>
 
 int MenuInicial(FILE** arquivoDeEntrada);
-int MenuImpressaoProcesso();
 FILE* abreArquivoRead(char* nomeArquivo);
-
-void flag(int i);
